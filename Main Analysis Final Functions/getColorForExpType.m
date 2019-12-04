ebd336f01a75ff6b62356eba4a6691a158fdf9ef
Niv Ierushalmi <nivieru@campus.technion.ac.il>
2019-12-04 14:19:51 +0200

    Add fixed gray color for several control conditions

--- Main Analysis Final Functions/getColorForExpType.m
+++ Main Analysis Final Functions/getColorForExpType.m
@@ -6,6 +6,12 @@ function color = getColorForExpType(expType)
             color = [200 100 100]/255;
         case 104 % Rambam 6 80%
             color = [100 100 200]/255;
+        case 10 % Rambam 10 80%
+            color = [128 128 128]/255; %%% Gray
+        case 100 % Rambam 10 80%
+            color = [200 100 100]/255; %%% Gray
+        case 101 % Rambam 10 80%
+            color = [100 100 200]/255; %%% Gray
         otherwise
             color = getcolorfromindex(gca,expType);
     end
